a7fdce4e7e6fab5ba9cfcdb1825c8c2d92324813
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-10-07 05:33:21 +0300

    Separate private and public interface

--- lua/lf.lua
+++ lua/lf.lua
@@ -6,7 +6,7 @@ local State = {
   Opened = 2,
 }
 
-local Lf = {
+local Private = {
   state = State.Closed,
   bufnr = nil,
   winid = nil,
@@ -20,7 +20,9 @@ local Lf = {
   },
 }
 
-function Lf:_close()
+local Public = {}
+
+function Private:close()
   self.state = State.Closed
   if api.nvim_win_is_valid(self.winid) then
     api.nvim_win_close(self.winid, true)
@@ -31,99 +33,99 @@ function Lf:_close()
   end
 end
 
-function Lf:open()
-  if self.state == State.Opened then
-    self:_hide()
+function Public:open()
+  if Private.state == State.Opened then
+    Private:hide()
   else
-    self.prev_winid = api.nvim_get_current_win()
+    Private.prev_winid = api.nvim_get_current_win()
 
-    if self.state == State.Closed then
-      self.bufnr = api.nvim_create_buf(false, true)
+    if Private.state == State.Closed then
+      Private.bufnr = api.nvim_create_buf(false, true)
     end
 
     local opts = {
       relative = 'editor',
-      col = math.floor((1 - self.config.width) / 2 * vim.o.columns),
-      row = math.floor((1 - self.config.height) / 2 * vim.o.lines),
-      width = math.floor(self.config.width * vim.o.columns),
-      height = math.floor(self.config.height * vim.o.lines),
-      border = self.config.border,
+      col = math.floor((1 - Private.config.width) / 2 * vim.o.columns),
+      row = math.floor((1 - Private.config.height) / 2 * vim.o.lines),
+      width = math.floor(Private.config.width * vim.o.columns),
+      height = math.floor(Private.config.height * vim.o.lines),
+      border = Private.config.border,
     }
-    self.winid = api.nvim_open_win(self.bufnr, true, opts)
-    api.nvim_win_set_option(self.winid, 'winhl', 'NormalFloat:LfNormal,FloatBorder:LfBorder')
-    api.nvim_win_set_option(self.winid, 'sidescrolloff', 0)
-    api.nvim_win_set_option(self.winid, 'number', false)
+    Private.winid = api.nvim_open_win(Private.bufnr, true, opts)
+    api.nvim_win_set_option(Private.winid, 'winhl', 'NormalFloat:LfNormal,FloatBorder:LfBorder')
+    api.nvim_win_set_option(Private.winid, 'sidescrolloff', 0)
+    api.nvim_win_set_option(Private.winid, 'number', false)
 
-    if self.state == State.Closed then
-      vim.fn.termopen(self.cmd, {
+    if Private.state == State.Closed then
+      vim.fn.termopen(Private.cmd, {
         on_exit = function()
-          self:_close()
+          Private:close()
         end,
       })
-      api.nvim_buf_set_option(self.bufnr, 'bufhidden', 'hide')
-      api.nvim_buf_set_option(self.bufnr, 'filetype', 'lf')
-      api.nvim_buf_set_name(self.bufnr, 'Lf')
+      api.nvim_buf_set_option(Private.bufnr, 'bufhidden', 'hide')
+      api.nvim_buf_set_option(Private.bufnr, 'filetype', 'lf')
+      api.nvim_buf_set_name(Private.bufnr, 'Lf')
     end
 
-    api.nvim_win_set_cursor(self.winid, { 1, 0 })
+    api.nvim_win_set_cursor(Private.winid, { 1, 0 })
     vim.schedule(vim.cmd.startinsert)
-    self.state = State.Opened
+    Private.state = State.Opened
   end
 end
 
-function Lf.setup(opts)
-  Lf.config = vim.tbl_extend('force', Lf.config, opts or {})
+function Public.setup(opts)
+  Private.config = vim.tbl_extend('force', Private.config, opts or {})
 
   if opts and opts.lfrc then
-    Lf.cmd = 'lf -config ' .. opts.lfrc
+    Private.cmd = 'lf -config ' .. opts.lfrc
   else
-    Lf.cmd = 'lf'
+    Private.cmd = 'lf'
   end
 
   api.nvim_create_user_command('Lf', function()
-    Lf:open()
+    Public:open()
   end, { nargs = 0, desc = 'Open lf' })
 
   api.nvim_set_hl(0, 'LfNormal', { link = 'NormalFloat', default = true })
   api.nvim_set_hl(0, 'LfBorder', { link = 'FloatBorder', default = true })
 end
 
-function Lf:_edit_file(path, hide)
+function Public:edit_file(path, hide)
   api.nvim_cmd({
     cmd = 'edit',
     args = { path },
   }, {})
-  local bufnr = api.nvim_win_get_buf(self.winid)
-  if api.nvim_win_is_valid(self.prev_winid) then
-    local nu = api.nvim_win_get_option(self.prev_winid, 'number')
-    local siso = api.nvim_win_get_option(self.prev_winid, 'sidescrolloff')
+  local bufnr = api.nvim_win_get_buf(Private.winid)
+  if api.nvim_win_is_valid(Private.prev_winid) then
+    local nu = api.nvim_win_get_option(Private.prev_winid, 'number')
+    local siso = api.nvim_win_get_option(Private.prev_winid, 'sidescrolloff')
 
-    api.nvim_win_set_buf(self.prev_winid, bufnr)
+    api.nvim_win_set_buf(Private.prev_winid, bufnr)
 
-    api.nvim_win_set_option(self.prev_winid, 'number', nu)
-    api.nvim_win_set_option(self.prev_winid, 'sidescrolloff', siso)
+    api.nvim_win_set_option(Private.prev_winid, 'number', nu)
+    api.nvim_win_set_option(Private.prev_winid, 'sidescrolloff', siso)
   end
-  if hide or self.config.hide then
-    self:_hide()
+  if hide or Private.config.hide then
+    Private:hide()
   else
-    api.nvim_win_set_buf(self.winid, self.bufnr)
+    api.nvim_win_set_buf(Private.winid, Private.bufnr)
   end
 end
 
-function Lf:_hide()
-  self.state = State.Hidden
-  if api.nvim_win_is_valid(self.winid) then
-    api.nvim_win_close(self.winid, true)
+function Public:hide()
+  Private.state = State.Hidden
+  if api.nvim_win_is_valid(Private.winid) then
+    api.nvim_win_close(Private.winid, true)
   end
 end
 
-function Lf:_cd(path)
+function Public:cd(path)
   api.nvim_cmd({
     cmd = 'cd',
     args = { path },
     mods = { silent = true },
   }, {})
-  api.nvim_buf_set_name(self.bufnr, 'Lf')
+  api.nvim_buf_set_name(Private.bufnr, 'Lf')
 end
 
-return Lf
+return Public
