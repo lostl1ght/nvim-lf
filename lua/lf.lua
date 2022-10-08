2554d6d2b3f2e0475e54a2b92364c339564f7520
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-10-08 19:19:11 +0300

    Figure out supplying path and add docs

--- lua/lf.lua
+++ lua/lf.lua
@@ -1,52 +1,69 @@
 local api = vim.api
 
+---@enum State
 local State = {
   Closed = 0,
   Hidden = 1,
   Opened = 2,
 }
 
-local Private = {
-  state = State.Closed,
-  bufnr = nil,
-  winid = nil,
-  prev_winid = nil,
-  cmd = nil,
-  config = {
-    hide = false,
-    width = 0.9,
-    height = 0.9,
-    border = 'none',
-  },
+---@class DefaultConfig
+local DefaultConfig = {
+  hide = false,
+  width = 0.9,
+  height = 0.9,
+  border = 'none',
+  lfrc = '',
 }
 
-local Public = {}
+---@class Private
+local Private = {
+  state = State.Closed,
+  bufnr = -1,
+  winid = -1,
+  prev_winid = -1,
+  cmd = 'lf ',
+  config = DefaultConfig,
+}
 
-function Private:close()
-  self.state = State.Closed
-  if api.nvim_win_is_valid(self.winid) then
-    api.nvim_win_close(self.winid, true)
-  end
+---Delete terminal buffer
+function Private:delete_buffer()
   if api.nvim_buf_is_valid(self.bufnr) then
     api.nvim_buf_set_var(self.bufnr, 'bufhidden', 'wipe')
     api.nvim_buf_delete(self.bufnr, { force = true })
   end
+  self.bufnr = -1
 end
 
-function Private:create_buffer()
-  self.bufnr = api.nvim_create_buf(false, true)
-  api.nvim_win_set_buf(self.winid, self.bufnr)
-  vim.fn.termopen(self.cmd, {
-    on_exit = function()
-      self:close()
-    end,
-  })
-  api.nvim_buf_set_option(self.bufnr, 'bufhidden', 'hide')
-  api.nvim_buf_set_option(self.bufnr, 'filetype', 'lf')
+---Delete floating window
+function Private:delete_window()
+  if api.nvim_win_is_valid(self.winid) then
+    api.nvim_win_close(self.winid, true)
+  end
+  self.winid = -1
 end
 
+---Create a buffer and open the terminal
+---@param path string|nil
+function Private:create_buffer(path)
+  if not api.nvim_buf_is_valid(self.bufnr) then
+    self.bufnr = api.nvim_create_buf(false, true)
+    api.nvim_win_set_buf(self.winid, self.bufnr)
+    vim.fn.termopen(self.cmd .. (path or ''), {
+      on_exit = function()
+        self:delete_buffer()
+        self.state = State.Closed
+      end,
+    })
+    api.nvim_buf_set_option(self.bufnr, 'bufhidden', 'hide')
+    api.nvim_buf_set_option(self.bufnr, 'filetype', 'lf')
+  end
+end
+
+---Set buffer, cursor, start insert mode
 function Private:post_open_setup()
   api.nvim_win_set_buf(self.winid, self.bufnr)
+  -- Fixes the window sliding to the left
   api.nvim_win_set_cursor(self.winid, { 1, 0 })
   vim.schedule(vim.cmd.startinsert)
 end
@@ -67,34 +84,74 @@ function Private:create_window()
   api.nvim_win_set_option(Private.winid, 'number', false)
 end
 
-function Public:open()
-  if Private.state ~= State.Opened then
-    Private:create_window()
-    if Private.state == State.Closed then
-      Private:create_buffer()
-    end
-    Private:post_open_setup()
-    Private.state = State.Opened
-  end
-end
+-- Public interface
 
+---@class Public
+local Public = {}
+
+---Setup the plugin
+---@param opts DefaultConfig|nil configuration
 function Public.setup(opts)
   Private.config = vim.tbl_extend('force', Private.config, opts or {})
 
   if opts and opts.lfrc then
-    Private.cmd = 'lf -config ' .. opts.lfrc
-  else
-    Private.cmd = 'lf'
+    Private.cmd = 'lf -config ' .. opts.lfrc .. ' '
   end
 
-  api.nvim_create_user_command('Lf', function()
-    Public:open()
-  end, { nargs = 0, desc = 'Open lf' })
+  api.nvim_create_user_command('LfOpen', function(arg)
+    local path
+    if arg.args ~= '' then
+      path = arg.args
+    end
+    Public:open(path)
+  end, { nargs = '?', complete = 'dir', desc = 'Open lf' })
+
+  api.nvim_create_user_command('LfHide', function()
+    Public:hide()
+  end, { nargs = 0, desc = 'Hide lf' })
+
+  api.nvim_create_user_command('LfToggle', function()
+    if Private.state == State.Opened then
+      Public:hide()
+    else
+      Public:open(nil)
+    end
+  end, { nargs = 0, desc = 'Toggle lf window' })
 
   api.nvim_set_hl(0, 'LfNormal', { link = 'NormalFloat', default = true })
   api.nvim_set_hl(0, 'LfBorder', { link = 'FloatBorder', default = true })
 end
 
+---Open lf
+---If `path` is `nil` then open in cwd
+---else (re)open in `path`
+---@param path string|nil path to a folder
+function Public:open(path)
+  if path then
+    Private:delete_buffer()
+    -- Needed so on_exit does not close a new lf instance
+    vim.wait(1000, function()
+      return Private.state == State.Closed
+    end, 50)
+  end
+  if Private.state ~= State.Opened then
+    Private:create_window()
+    Private:create_buffer(path)
+    Private:post_open_setup()
+    Private.state = State.Opened
+  end
+end
+
+---Hide lf window
+function Public:hide()
+  Private:delete_window()
+  Private.state = State.Hidden
+end
+
+---Edit file under cursor in lf
+---`Should only be used with remote capabilities from lfrc`
+---@param path string path to a file
+---@param hide boolean whether to hide the window
 function Public:edit_file(path, hide)
   api.nvim_cmd({
     cmd = 'edit',
@@ -111,19 +168,15 @@ function Public:edit_file(path, hide)
     api.nvim_win_set_option(Private.prev_winid, 'sidescrolloff', siso)
   end
   if hide or Private.config.hide then
-    Private:hide()
+    Public:hide()
   else
     api.nvim_win_set_buf(Private.winid, Private.bufnr)
   end
 end
 
-function Public:hide()
-  Private.state = State.Hidden
-  if api.nvim_win_is_valid(Private.winid) then
-    api.nvim_win_close(Private.winid, true)
-  end
-end
-
+---Change nvim's cwd
+---`Should only be called with remote capabilities from lfrc`
+---@param path string directory
 function Public:cd(path)
   api.nvim_cmd({
     cmd = 'cd',
